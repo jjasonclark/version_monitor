8d1ec1d056dce2b377b43d08f8f2f56e0dde140c
Jason Clark <jason@jjcconsultingllc.com>
2015-11-01 08:20:47 -0800

    wip

--- src/main.go
+++ src/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("%s\nVersion: %s SHA %s\n\n", AppName, Version, BuildSHA)
+
+	resp, err := http.Get("http://www.par8o.com/revision")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("https://github.com/par8o/par8o/commit/%s\n", body)
 }
