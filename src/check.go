3b136379ae603d5e1d3df35e3644a5db418cbb02
Jason Clark <jason@jjcconsultingllc.com>
2015-11-15 14:31:02 -0800

    finish basic features

---
+++ src/check.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"net/http"
+)
+
+var errFetchFail = errors.New("Failed to fetch SHA")
+
+func fetchVersionSha(source string) (string, error) {
+	resp, err := http.Get(source)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", errFetchFail
+	}
+	var body [40]byte
+	n, err := resp.Body.Read(body[0:])
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+	if n < 1 {
+		return "", errFetchFail
+	}
+	return string(body[:n]), nil
+}
+
+type checkResult struct {
+	Check
+	Err    error
+	Result string
+}
+
+func (left checkResult) sameAs(right checkResult) bool {
+	return left.Result == right.Result
+}
+
+type Check struct {
+	Name   string
+	Source string
+	Verify string
+}
+
+func (c Check) check(results chan<- checkResult) {
+	sha, err := fetchVersionSha(c.Source)
+	results <- checkResult{c, err, sha}
+}
+
+func queueAllChecks(checks []Check, resultsChannel chan<- checkResult) {
+	for _, c := range checks {
+		go c.check(resultsChannel)
+	}
+}
